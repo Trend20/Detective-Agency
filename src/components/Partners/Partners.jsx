9aee79ee1ecc61e6e2532ea67198465f40f436bf
Trend20 <enockomondi305@gmail.com>
2022-09-16 09:12:44 +0300

    partners component

--- src/components/Partners/Partners.jsx
+++ src/components/Partners/Partners.jsx
@@ -2,23 +2,23 @@ import './Partners.css'
 
 const statsArray = [
   {
-    val: 878,
+    val: '878',
     desc: 'Cases solved in 2019'
   },
   {
-    val: 17,
+    val:' 17+',
     desc: 'Detectives'
   },
   {
-    val: 10,
+    val: '10',
     desc: 'Labs around the world'
   },
   {
-    val: 96,
+    val: '96%',
     desc: 'Successful outcomes'
   },
   {
-    val: 20,
+    val: '20',
     desc: 'Clients contact us every week'
   }
 ]
@@ -29,17 +29,21 @@ const Partners = () => {
       <div className="partners-description">
         <p>Numbers</p>
         <h1>We're Proud of</h1>
-        <p>
+        <p id='mission'>
           Our mission is to provide outstanding and reliable professional <strong>private investigation services</strong> anywhere in the world. 
           We work around the clock to report our findings in a timely and accountable manner. 
         </p>
       </div>
       <div className="partners-stats">
-        <div className="stat">
-          <h3>878</h3>
-          <hr />
-          <p>Cases solved in 2019</p>
-        </div>
+        {
+          statsArray.map((stat, index) =>(
+            <div className="stat" key={index}>
+              <h3>{stat.val}</h3>
+              <hr />
+              <p>{stat.desc}</p>
+            </div>
+          ))
+        }
       </div>
     </div>
   )
