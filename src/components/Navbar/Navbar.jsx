706742331e703233d0c9e5e4bd4378f9119c75a7
Trend20 <enockomondi305@gmail.com>
2022-10-22 17:56:24 +0300

    navbar fix

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,19 +1,48 @@
+import { useState } from 'react';
 import './Navbar.css';
-import { FaFacebookF, FaInstagram, FaYoutube} from 'react-icons/fa';
+import {Link } from 'react-scroll';
+import { AiOutlineClose } from 'react-icons/ai';
+import { FaFacebookF, FaInstagram, FaYoutube, FaBars} from 'react-icons/fa';
 
-const navItems = ['Services', 'About Us', 'Partners', 'Testimonials', 'Contacts'];
+const navItems = [
+  {
+    val: 'Services',
+    sectionID: "services"
+  }, 
+  {
+    val: 'About Us',
+    sectionID: "about"
+  }, 
+  {
+    val: 'Partners',
+    sectionID: "partner"
+  },
+  {
+    val: 'Testimonials',
+    sectionID: "testimonies"
+  }, 
+  {
+    val: 'Contacts',
+    sectionID: "contact"
+  }
+];
 
 const Navbar = () => {
+
+  const [click, setClick] = useState(false);
+	const handleClick = () => setClick(!click);
+	const closeMobileMenu = () => setClick(false);
   return (
+  <>
     <div className='header'>
       <div className="logo">
         <img src="/img/logo.svg" alt="logo" />
       </div>
       <div className="navbar">
-        <ul>
+        <ul className={click ? 'nav-options active' : 'nav-options'} id="navbar">
           {
             navItems.map((item, index) =>(
-              <li key={index}><span>0{index + 1}.</span>{item}</li>
+              <li key={index}><span><Link to="about" spy={true} smooth={true}>0{index + 1}.</Link></span>{item}</li>
             ))
           }
         </ul>
@@ -22,8 +51,12 @@ const Navbar = () => {
           <FaInstagram  className='icon'    size="25px" color="#1e1e28" fill='#1e1e28'/>
           <FaYoutube className='icon'    size="25px" color="#1e1e28" fill='#1e1e28'/>
         </div>
+        <div className="toggle mobile-menu" onClick={handleClick}>
+					{click ? <AiOutlineClose className="menu-icon" /> : <FaBars className="menu-icon" />}
+				</div>
       </div>
     </div>
+  </>
   )
 }
 
